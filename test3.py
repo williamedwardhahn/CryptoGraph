0d7656a93e0204052a86bcfea2c7200213f55c3b
normonics <JNorman@Joes-Mac-3.local>
2014-01-31 03:23:59 -0500

    get some pairs

--- test3.py
+++ test3.py
@@ -2,10 +2,13 @@ import urllib2 as URL
 import json as JS
  
 coinlist = URL.urlopen('http://www.cryptocoincharts.info/v2/api/listCoins')
+toptenpairs = URL.urlopen('http://www.cryptocoincharts.info/v2/api/tradingPairs')
 
 rawnodelist = coinlist.read()
-
 nodelist = JS.loads(rawnodelist)
+
+rawtoptenpairs = toptenpairs.read()
+toptenpairslist = JS.loads(rawtoptenpairs)
 
 def getidlist():
     idlist = []
@@ -13,9 +16,19 @@ def getidlist():
         idlist.append(nodelist[i]['id'])
     return idlist
 
+def gettoptenpairsidlist():
+    toptenpairsidlist = []
+    for i in range(len(toptenpairslist)):
+        toptenpairsidlist.append(toptenpairslist[i]['id'])
+    return toptenpairsidlist    
+    
+    
+
 
 ids = getidlist()
+pairsids = gettoptenpairsidlist()
 
 print ids
+print pairsids
 
 
